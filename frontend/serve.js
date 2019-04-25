df6c5104074f6765fa208257b8d49ced8d287aee
Tiemen <Tiemen2@live.nl>
2019-04-25 23:11:35 +0200

    [frontend] change host

--- frontend/serve.js
+++ frontend/serve.js
@@ -5,7 +5,7 @@ const express = require('express');
 const bundler = new Bundler('src/index.html', { cache: false });
 const app = express();
 
-app.use('/api', proxy({ target: 'http://localhost:8686' }));
+app.use('/api', proxy({ target: 'http://0.0.0.0:8686' }));
 
 app.use(bundler.middleware());
 
