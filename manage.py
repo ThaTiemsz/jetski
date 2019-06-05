8114dfde7abae1ec46825f98572920316a4cc901
Tiemen <Tiemen2@live.nl>
2019-06-05 03:15:53 +0200

    gevent thing

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@
 from gevent import monkey; monkey.patch_all()
 
 from werkzeug.serving import run_with_reloader
-from gevent import wsgi
+from gevent import .pywsgi
 from rowboat import ENV
 from rowboat.web import rowboat
 from rowboat.sql import init_db
