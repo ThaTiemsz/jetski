576a16b292ba7339f2d4224879e7bd6d2324a01d
Tiemen <Tiemen2@live.nl>
2019-11-22 18:55:48 +0100

    [core] don't repeat update_guild_syncs

--- rowboat/plugins/core.py
+++ rowboat/plugins/core.py
@@ -387,7 +387,7 @@ class CorePlugin(Plugin):
             embed.add_field(name='Gateway Version', value='v{}'.format(event.version), inline=False)
             embed.add_field(name='Session ID', value=event.session_id, inline=False)
 
-    @Plugin.schedule(45, init=False)
+    @Plugin.schedule(45, init=False, repeat=False)
     def update_guild_syncs(self):
         if len(self.guild_sync) == 0:
             return
